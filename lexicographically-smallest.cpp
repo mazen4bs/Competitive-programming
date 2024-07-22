4c189419db077106346df15caea555158376d9ab
Mazen <128807230+mazen4bs@users.noreply.github.com>
2024-07-22 17:49:38 +0300

    Add files via upload

---
+++ lexicographically-smallest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+    for (int i = 0; i < n - 1; i++) {
+        if (s[i] > s[i + 1]) {
+            s.erase(i, 1);
+            cout << s << endl;
+            return 0;
+        }
+    }
+    s.pop_back();
+    cout << s << endl;
+    return 0;
+}
